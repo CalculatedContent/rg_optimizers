431204af10d82b3102c19fc22587a93f03160c85
Charles Martin <charlesmartin14@gmail.com>
2026-08-08 14:48:38 -0700

    Expose full matrix-log support API

--- optimizers/full_matrix_log_rg/full_matrix_log_rg/__init__.py
+++ optimizers/full_matrix_log_rg/full_matrix_log_rg/__init__.py
@@ -1,5 +1,24 @@
-from .config import FullMatrixLogConfig
-from .geometry import MatrixLogGeometry, ProjectionResult, full_matrix_log_geometry, remove_inward_matrix_log_flow
-from .support import SupportCheckpoint, analyze_supports
+"""Full matrix-log RG optimizer."""
+
+from .config import CorrectionMode, FullMatrixLogConfig
+from .geometry import (
+    MatrixLogGeometry,
+    ProjectionResult,
+    full_matrix_log_geometry,
+    remove_inward_matrix_log_flow,
+)
+from .support import MatrixLogSupport, SupportCheckpoint, analyze_supports
 from .wrapper import FullMatrixLogRG
-__all__=['FullMatrixLogConfig','FullMatrixLogRG','MatrixLogGeometry','ProjectionResult','SupportCheckpoint','analyze_supports','full_matrix_log_geometry','remove_inward_matrix_log_flow']
+
+__all__ = [
+    "CorrectionMode",
+    "FullMatrixLogConfig",
+    "FullMatrixLogRG",
+    "MatrixLogGeometry",
+    "MatrixLogSupport",
+    "ProjectionResult",
+    "SupportCheckpoint",
+    "analyze_supports",
+    "full_matrix_log_geometry",
+    "remove_inward_matrix_log_flow",
+]
